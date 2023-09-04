f24412af7c796687332e20d530e64d5a62689648
Наумов Алексей <work@anaumov.com>
2023-09-04 11:09:37 +0300

    No changes

--- index.ts
+++ index.ts
@@ -63,6 +63,6 @@ app.get('/images/:imageName', (req, res) => {
 });
 
 // Запускаем сервер на порту 3000
-app.listen(3000, () => {
+app.listen(3000, () => { 
     console.log('Server started on http://localhost:3000');
 });
